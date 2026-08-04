70498fa756e6a1018955cd819143da7d14c0bc55
liebeSonne <>
2026-08-04 12:23:11 +0300

    v1 - добавил миграции хранения данных файлов

---
+++ migrations/20260804121726_add_files_table.down.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS idx_file_chunk_file_id;
+DROP INDEX IF EXISTS idx_file_user_id;
+DROP TABLE IF EXISTS file_chunk;
+DROP TABLE IF EXISTS file;
---
+++ migrations/20260804121726_add_files_table.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE file (
+    id           UUID          NOT NULL PRIMARY KEY,
+    user_id      UUID          NOT NULL,
+    name         VARCHAR(1024) NOT NULL,
+    mime_type    VARCHAR(256)  NOT NULL,
+    size         BIGINT        NOT NULL,
+    chunks_count INT           NOT NULL,
+    status       INT           NOT NULL DEFAULT 0,
+    created_at   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT   fk_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE file_chunk (
+    id          UUID    NOT NULL PRIMARY KEY,
+    file_id     UUID    NOT NULL,
+    chunk_index INT     NOT NULL,
+    uploaded    BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT  fk_file_id FOREIGN KEY (file_id) REFERENCES file (id) ON DELETE CASCADE,
+    UNIQUE (file_id, chunk_index)
+);
+
+CREATE INDEX file_user_id_idx ON file (user_id);
+CREATE INDEX file_chunk_file_id_idx ON file_chunk (file_id);
